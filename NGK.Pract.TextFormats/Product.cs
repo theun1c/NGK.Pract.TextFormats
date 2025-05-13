210d6ecacdbdaafb95b45c82b67fe3fe03268f03
theun1c <belovdima1122@yandex.ru>
2025-05-13 17:26:59 +0300

    init

---
+++ NGK.Pract.TextFormats/Product.cs
@@ -0,0 +1,12 @@
+﻿//1.	Управление запасами: Считывание и обработка данных о товарах на складе (идентификатор, название, количество, цена, категория)
+namespace NGK.Pract.TextFormats
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Count { get; set; }
+        public int Cost { get; set; }
+        public string Category { get; set; }
+    }
+}
