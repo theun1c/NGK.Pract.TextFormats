d992c48aeeb62034a036fc7e9d2fc3406c9c10e8
theun1c <belovdima1122@yandex.ru>
2025-05-13 18:17:33 +0300

    add write + read methods

--- NGK.Pract.TextFormats/Product.cs
+++ NGK.Pract.TextFormats/Product.cs
@@ -9,7 +9,12 @@ namespace NGK.Pract.TextFormats
         public int Id { get; set; }
         public string Name { get; set; }
         public int Count { get; set; }
-        public int Cost { get; set; }
+        public int Price { get; set; }
         public string Category { get; set; }
+
+        public override string ToString()
+        {
+            return $"id: {Id} \nname: {Name} \ncount: {Count} \nprice: {Price} \ncategory: {Category}";
+        }
     }
 }
